42d820b1ddae73ca86d57faa737bb1fae9efbe77
Luwyfel <luwyfelvoid@gmail.com>
2026-05-03 18:04:29 +0700

    Update flakevars.nix

--- lib/flakevars.nix
+++ lib/flakevars.nix
@@ -1,8 +1,8 @@
 rec {
   system = "x86_64-linux";
-  hostname = "Luwyfelvoid";
-  username = "luwyfel";
-  flake = "nappy";
+  hostname = "Luwyfel";
+  username = "luwy";
+  flake = "Nap";
   email = "luwyfelvoid@gmail.com";
   homeDir  = "/home/${username}";
   flakeDir = "${homeDir}/${flake}";
