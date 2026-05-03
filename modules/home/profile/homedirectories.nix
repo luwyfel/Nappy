49489e3332bb21d4926090e182cdd7f891cb2aba
Luwyfel <luwyfelvoid@gmail.com>
2026-05-03 22:05:14 +0700

    Update homedirectories.nix

--- modules/home/profile/homedirectories.nix
+++ modules/home/profile/homedirectories.nix
@@ -1,5 +1,7 @@
-{ ... }:
+{ config, ... }:
 {
-  home.file."Games/Internal".source = config.lib.file.mkOutOfStoreSymlink "/games/Internal";
-  home.file."Games/External".source = config.lib.file.mkOutOfStoreSymlink "/games/External";
+  home.file = {
+    "Games/Internal".source = config.lib.file.mkOutOfStoreSymlink "/games/Internal";
+    "Games/External".source = config.lib.file.mkOutOfStoreSymlink "/games/External";
+  };
 }
