4d2691a29831e9d21b8a8a2812b16507106eafd1
Luwyfel <luwyfelvoid@gmail.com>
2026-05-03 21:50:37 +0700

    Update filerules.nix

--- modules/system/base/filerules.nix
+++ modules/system/base/filerules.nix
@@ -1,7 +1,7 @@
 { username, ... }:
 {
   systemd.tmpfiles.rules = [
-    "d /games/Local 0755 ${username} users -"
+    "d /games/Internal 0755 ${username} users -"
     "d /games/External 0755 ${username} users -"
   ];
 }
