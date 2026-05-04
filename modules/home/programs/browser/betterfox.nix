69d439a0dd45d11ddfd835f1c91fa06dd2f524f5
Luwyfel <luwyfelvoid@gmail.com>
2026-05-04 10:23:01 +0700

    Update betterfox.nix

--- modules/home/programs/browser/betterfox.nix
+++ modules/home/programs/browser/betterfox.nix
@@ -5,7 +5,6 @@
   profiles.${username} = {
     enableAllSections = true;
     settings = {
-      fastfox.enable = true;
       peskyfox.enable = true;
       securefox.enable = true;
       smoothfox.natural-smooth-scrolling-v3.enable = true;
